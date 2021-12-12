5762144df5769eaf8b083db2376b1ac2cc494bc2
pauldes <paul.desigaud@wavestone.com>
2021-12-12 15:51:46 +0100

    Add titles

--- nba_mvp_predictor/web.py
+++ nba_mvp_predictor/web.py
@@ -2,13 +2,14 @@ import streamlit as st
 
 from nba_mvp_predictor import conf
 
-
 def run():
-    print(conf)
     st.set_page_config(
-        page_title=conf.web.page_title,
-        # page_icon="nba_mvp_predictor/static/favicon.ico",
+        page_title=conf.web.tab_title,
+        page_icon=":basketball:",
         layout="wide",
         initial_sidebar_state="auto",
     )
+    st.title(conf.web.page_title)
+    st.sidebar.markdown(conf.web.sidebar_top_text)
+    st.sidebar.markdown(conf.web.sidebar_bottom_text)
     st.title(conf.web.page_title)
