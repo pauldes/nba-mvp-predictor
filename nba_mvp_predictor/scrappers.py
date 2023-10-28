1f1f2b0824b8f4e913b1c3d0d13318531d08e2b8
pauldes <pauldesigaud@outlook.fr>
2023-10-28 11:34:31 +0200

    Wait times

--- nba_mvp_predictor/scrappers.py
+++ nba_mvp_predictor/scrappers.py
@@ -1,9 +1,9 @@
 import datetime
 from abc import ABC, abstractmethod
 import time
+from os import path
 
 import requests
-import yaml
 import pandas
 from bs4 import BeautifulSoup
 from basketball_reference_scrapper.seasons import get_standings
@@ -56,58 +56,65 @@ class BasketballReferenceScrapper(Scrapper):
             "./nba_mvp_predictor/team_names.yaml"
         )
 
-    @staticmethod
-    def retrieve_mvp_votes(season):
-        season = str(season)
+    @classmethod
+    def get_request(uri):
         root_url = "https://www.basketball-reference.com/"
-        url = f"{root_url}awards/awards_{season}.html"
+        url = path.join(root_url, uri)
+        # Wait some time to avoid BR anti-bot measures (HTTP 429)
+        utils.wait_random_time(2, 10)
         r = requests.get(url)
         if r.status_code == 200:
-            soup = BeautifulSoup(r.content, "html.parser")
-            table_mvp = soup.find("table", id="mvp")
-            table_nba_mvp = soup.find("table", id="nba_mvp")
-            if table_mvp is not None:
-                table = table_mvp
-            elif table_nba_mvp is not None:
-                table = table_nba_mvp
-            else:
-                raise Exception("No table found for MVP data for season", season)
-            data = pandas.read_html(str(table), header=1)[0]
-            data.columns = [str(col).upper() for col in data.columns]
-            data.loc[:, "SEASON"] = season
-            data = data.rename(columns={"SHARE": "MVP_VOTES_SHARE"})
-            data = data.rename(columns={"TM": "TEAM"})
-            data = data[["PLAYER", "TEAM", "SEASON", "MVP_VOTES_SHARE", "RANK"]]
-            data.loc[:, "PLAYER"] = data["PLAYER"].str.replace(
-                # "[^A-Za-z]", "", regex=True
-                "[ _'.*]",
-                "",
-                regex=True,
-            )
-            data.loc[:, "MVP_WINNER"] = False
-            data["RANK"] = (
-                data["RANK"]
-                .astype(str)
-                .str.replace("[^0-9]", "", regex=True)
-                .astype(int, errors="raise")
-            )
-            data.loc[data["RANK"] == 1, "MVP_WINNER"] = True
-            data.loc[:, "MVP_PODIUM"] = False
-            data.loc[data["RANK"].isin([1, 2, 3]), "MVP_PODIUM"] = True
-            data.loc[:, "MVP_CANDIDATE"] = True
-            data = data.drop("RANK", axis="columns")
-            return data
+            return r
         else:
+            logger.error("Failed to get %s", url)
             retry_after = r.headers["Retry-After"]
             if r.status_code == 429:
-                message = (
-                    f"(status code 429, too many requests, retry after {retry_after})"
-                )
+                message = f"(too many requests, retry after {retry_after})"
             else:
                 message = f"(status code {r.status_code}"
             raise ConnectionError(
                 f"Could not connect to BR and get data, status code : {message}"
             )
+
+    @classmethod
+    def retrieve_mvp_votes(cls, season):
+        season = str(season)
+        uri = f"awards/awards_{season}.html"
+        r = cls.get_request(uri)
+        soup = BeautifulSoup(r.content, "html.parser")
+        table_mvp = soup.find("table", id="mvp")
+        table_nba_mvp = soup.find("table", id="nba_mvp")
+        if table_mvp is not None:
+            table = table_mvp
+        elif table_nba_mvp is not None:
+            table = table_nba_mvp
+        else:
+            raise Exception("No table found for MVP data for season", season)
+        data = pandas.read_html(str(table), header=1)[0]
+        data.columns = [str(col).upper() for col in data.columns]
+        data.loc[:, "SEASON"] = season
+        data = data.rename(columns={"SHARE": "MVP_VOTES_SHARE"})
+        data = data.rename(columns={"TM": "TEAM"})
+        data = data[["PLAYER", "TEAM", "SEASON", "MVP_VOTES_SHARE", "RANK"]]
+        data.loc[:, "PLAYER"] = data["PLAYER"].str.replace(
+            # "[^A-Za-z]", "", regex=True
+            "[ _'.*]",
+            "",
+            regex=True,
+        )
+        data.loc[:, "MVP_WINNER"] = False
+        data["RANK"] = (
+            data["RANK"]
+            .astype(str)
+            .str.replace("[^0-9]", "", regex=True)
+            .astype(int, errors="raise")
+        )
+        data.loc[data["RANK"] == 1, "MVP_WINNER"] = True
+        data.loc[:, "MVP_PODIUM"] = False
+        data.loc[data["RANK"].isin([1, 2, 3]), "MVP_PODIUM"] = True
+        data.loc[:, "MVP_CANDIDATE"] = True
+        data = data.drop("RANK", axis="columns")
+        return data
 
     def get_mvp(self, subset_by_seasons: list = None):
         year = datetime.datetime.now().year
@@ -140,14 +147,13 @@ class BasketballReferenceScrapper(Scrapper):
         )
         return all_df
 
-    @staticmethod
-    def get_roster_stats_v2(season, stat_type):
+    @classmethod
+    def get_roster_stats_v2(cls, season, stat_type):
         """
         Return all players stats for one season.
         Season : end year of season, as int or str
         Available stat types (case insensitive) : 'totals', 'per_game', 'per_36min', 'per_100poss', 'advanced'
         """
-        root_url = "https://www.basketball-reference.com/"
         season = str(season)
         stat_type = str(stat_type).lower()
         url_mapper = {
@@ -158,38 +164,66 @@ class BasketballReferenceScrapper(Scrapper):
             "advanced": "advanced",
         }
         stat_type = url_mapper[stat_type]
-        url = f"{root_url}leagues/NBA_{season}_{stat_type}.html"
-        r = requests.get(url)
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.content, "html.parser")
-            table = soup.find("table")
-            data = pandas.read_html(str(table))[0]
-            data = data.loc[data.Player != "Player", :]
-            data.columns = [str(col).upper() for col in data.columns]
-            data.loc[:, "SEASON"] = season
-            data.loc[:, "PLAYER"] = data["PLAYER"].str.replace(
-                # "[^-'a-zA-ZÀ-ÿ]", "", regex=True
-                "[ _'.*]",
-                "",
-                regex=True,
-            )
-            data = data.rename(columns={"TM": "TEAM"})
-            data = data.drop("RK", axis="columns")
-            for col in data.columns:
-                if col.startswith("3P"):
-                    data[col] = data[col].fillna(0.0)
-            return data
+        uri = f"leagues/NBA_{season}_{stat_type}.html"
+        r = cls.get_request(uri)
+        soup = BeautifulSoup(r.content, "html.parser")
+        table = soup.find("table")
+        data = pandas.read_html(str(table))[0]
+        data = data.loc[data.Player != "Player", :]
+        data.columns = [str(col).upper() for col in data.columns]
+        data.loc[:, "SEASON"] = season
+        data.loc[:, "PLAYER"] = data["PLAYER"].str.replace(
+            # "[^-'a-zA-ZÀ-ÿ]", "", regex=True
+            "[ _'.*]",
+            "",
+            regex=True,
+        )
+        data = data.rename(columns={"TM": "TEAM"})
+        data = data.drop("RK", axis="columns")
+        for col in data.columns:
+            if col.startswith("3P"):
+                data[col] = data[col].fillna(0.0)
+        return data
+
+    @classmethod
+    def get_standings(cls, date=None):
+        """Ported from https://github.com/vishaalagartha/basketball_reference_scraper.
+
+
+        Args:
+            date (_type_, optional): _description_. Defaults to None.
+
+        Raises:
+            ConnectionError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if date is None:
+            date = datetime.now()
         else:
-            retry_after = r.headers["Retry-After"]
-            if r.status_code == 429:
-                message = (
-                    f"(status code 429, too many requests, retry after {retry_after})"
-                )
-            else:
-                message = f"(status code {r.status_code}"
-            raise ConnectionError(
-                f"Could not connect to BR and get data, status code : {message}"
-            )
+            date = pandas.to_datetime(date)
+        d = {}
+        uri = f"friv/standings.fcgi?month={date.month}&day={date.day}&year={date.year}"
+        r = cls.get_request(uri)
+
+        soup = BeautifulSoup(r.content, "html.parser")
+        e_table = soup.find("table", attrs={"id": "standings_e"})
+        w_table = soup.find("table", attrs={"id": "standings_w"})
+        e_df = pd.DataFrame(
+            columns=["TEAM", "W", "L", "W/L%", "GB", "PW", "PL", "PS/G", "PA/G"]
+        )
+        w_df = pd.DataFrame(
+            columns=["TEAM", "W", "L", "W/L%", "GB", "PW", "PL", "PS/G", "PA/G"]
+        )
+        if e_table and w_table:
+            e_df = pd.read_html(str(e_table))[0]
+            w_df = pd.read_html(str(w_table))[0]
+            e_df.rename(columns={"Eastern Conference": "TEAM"}, inplace=True)
+            w_df.rename(columns={"Western Conference": "TEAM"}, inplace=True)
+        d["EASTERN_CONF"] = e_df
+        d["WESTERN_CONF"] = w_df
+        return d
 
     def get_team_standings(self, subset_by_seasons: list = None):
         """Assumptions : the season is over by June 1st.
@@ -369,82 +403,63 @@ class BasketballReferenceScrapper(Scrapper):
         return full_df
 
     def get_team_standings_on_date(self, day: int, month: int, year: int):
-        root_url = "https://www.basketball-reference.com/"
-        url = f"{root_url}friv/standings.fcgi?month={month}&day={day}&year={year}&lg_id=NBA"
-        logger.debug(url)
-        r = requests.get(url)
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.content, "html.parser")
-            table_east = soup.find("table", {"id": "standings_e"})
-            data_east = pandas.read_html(str(table_east))[0]
-            table_west = soup.find("table", {"id": "standings_w"})
-            data_west = pandas.read_html(str(table_west))[0]
+        uri = f"friv/standings.fcgi?month={month}&day={day}&year={year}&lg_id=NBA"
+        r = self.get_request(uri)
 
-            results = {
-                "West": data_west,
-                "East": data_east,
-            }
+        soup = BeautifulSoup(r.content, "html.parser")
+        table_east = soup.find("table", {"id": "standings_e"})
+        data_east = pandas.read_html(str(table_east))[0]
+        table_west = soup.find("table", {"id": "standings_w"})
+        data_west = pandas.read_html(str(table_west))[0]
 
-            dfs = []
+        results = {
+            "West": data_west,
+            "East": data_east,
+        }
 
-            for conference, data in results.items():
-                logger.debug(f"Standings data columns: {', '.join(data.columns)}")
-                data = data.dropna(axis="index", how="any")
-                logger.debug(
-                    f"First column name before renaming: {data.columns.values[0]}"
-                )
-                data = data.rename(columns={data.columns[0]: "TEAM"})
-                data.loc[:, "TEAM"] = (
-                    data["TEAM"].str.upper().str.replace("[^A-Z]", "", regex=True)
-                )
-                team_names = {}
-                for raw, short in self.team_names.items():
-                    raw = "".join(filter(str.isalpha, raw)).upper()
-                    team_names[raw] = short
-                data = data[~data["TEAM"].str.contains("DIVISION")]
+        dfs = []
 
-                data = data.sort_values(by="W/L%", ascending=False)
-                data = data.reset_index(drop=True)
-                data.loc[:, "CONF_RANK"] = data.index + 1
-                # data.loc[:, "CONF_RANK"] = data["W/L%"].rank(ascending=False, method='max')
-
-                logger.debug(f"Conference : {conference}")
-                data.loc[:, "CONF"] = (
-                    conference.replace(" ", "_").upper().replace("CONFERENCE", "CONF")
-                )
-                unmapped_teams = [
-                    team
-                    for team in data["TEAM"].unique()
-                    if team not in team_names.keys()
-                ]
-                data.loc[:, "TEAM"] = data["TEAM"].map(team_names)
-                if data["TEAM"].isna().sum() > 0:
-                    raise ValueError("Unknown/unmapped teams : %s", unmapped_teams)
-                data["GB"] = (
-                    data["GB"].str.replace("—", "0.0").astype(float, errors="raise")
-                )
-                data = data.astype(
-                    {
-                        "W": "int32",
-                        "L": "int32",
-                        "CONF_RANK": "int32",
-                        "W/L%": "float64",
-                    }
-                )
-                data = data.set_index("TEAM", drop=True)
-                dfs.append(data)
-            all_conf_df = pandas.concat(
-                dfs, join="outer", axis="index", ignore_index=False
+        for conference, data in results.items():
+            logger.debug(f"Standings data columns: {', '.join(data.columns)}")
+            data = data.dropna(axis="index", how="any")
+            logger.debug(f"First column name before renaming: {data.columns.values[0]}")
+            data = data.rename(columns={data.columns[0]: "TEAM"})
+            data.loc[:, "TEAM"] = (
+                data["TEAM"].str.upper().str.replace("[^A-Z]", "", regex=True)
             )
-            return all_conf_df
-        else:
-            retry_after = r.headers["Retry-After"]
-            if r.status_code == 429:
-                message = (
-                    f"(status code 429, too many requests, retry after {retry_after})"
-                )
-            else:
-                message = f"(status code {r.status_code}"
-            raise ConnectionError(
-                f"Could not connect to BR and get data, status code : {message}"
+            team_names = {}
+            for raw, short in self.team_names.items():
+                raw = "".join(filter(str.isalpha, raw)).upper()
+                team_names[raw] = short
+            data = data[~data["TEAM"].str.contains("DIVISION")]
+
+            data = data.sort_values(by="W/L%", ascending=False)
+            data = data.reset_index(drop=True)
+            data.loc[:, "CONF_RANK"] = data.index + 1
+            # data.loc[:, "CONF_RANK"] = data["W/L%"].rank(ascending=False, method='max')
+
+            logger.debug(f"Conference : {conference}")
+            data.loc[:, "CONF"] = (
+                conference.replace(" ", "_").upper().replace("CONFERENCE", "CONF")
             )
+            unmapped_teams = [
+                team for team in data["TEAM"].unique() if team not in team_names.keys()
+            ]
+            data.loc[:, "TEAM"] = data["TEAM"].map(team_names)
+            if data["TEAM"].isna().sum() > 0:
+                raise ValueError("Unknown/unmapped teams : %s", unmapped_teams)
+            data["GB"] = (
+                data["GB"].str.replace("—", "0.0").astype(float, errors="raise")
+            )
+            data = data.astype(
+                {
+                    "W": "int32",
+                    "L": "int32",
+                    "CONF_RANK": "int32",
+                    "W/L%": "float64",
+                }
+            )
+            data = data.set_index("TEAM", drop=True)
+            dfs.append(data)
+        all_conf_df = pandas.concat(dfs, join="outer", axis="index", ignore_index=False)
+        return all_conf_df
--- nba_mvp_predictor/utils.py
+++ nba_mvp_predictor/utils.py
@@ -1,5 +1,7 @@
 import yaml
 import box
+import random
+import time
 
 
 def get_dict_from_yaml(yaml_path: str):
@@ -7,3 +9,7 @@ def get_dict_from_yaml(yaml_path: str):
         conf_dict = yaml.safe_load(f)
     conf_dict = box.Box(conf_dict, default_box=True, default_box_attr=None)
     return conf_dict
+
+
+def wait_random_time(min, max) -> None:
+    time.sleep(random.randint(min, max))
