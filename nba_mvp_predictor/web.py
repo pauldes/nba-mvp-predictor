d83530710a0fabaaad16610770244c56bc7c2036
pauldes <pauldesigaud@outlook.fr>
2024-11-11 23:54:28 +0100

    Bump artifact season

--- nba_mvp_predictor/web.py
+++ nba_mvp_predictor/web.py
@@ -103,7 +103,7 @@ def download_performances():
 
 @st.cache_data(ttl=3600)  # 1h cache
 def download_predictions():
-    date, url = artifacts.get_last_artifact("predictions-2024.csv")
+    date, url = artifacts.get_last_artifact("predictions-2025.csv")
     logger.debug(f"Downloading predictions from {url}")
     download.download_data_from_url_to_file(
         url, "./data/predictions-artifact.csv.zip", auth=artifacts.get_github_auth()
@@ -112,10 +112,10 @@ def download_predictions():
 
 @st.cache_data(ttl=3600)  # 1h cache
 def download_shap_values():
-    date, url = artifacts.get_last_artifact("shap_values-2024.csv")
+    date, url = artifacts.get_last_artifact("shap_values-2025.csv")
     logger.debug(f"Downloading shap values from {url}")
     download.download_data_from_url_to_file(
-        url, "./data/shap_values-2024.csv.zip", auth=artifacts.get_github_auth()
+        url, "./data/shap_values-2025.csv.zip", auth=artifacts.get_github_auth()
     )
 
 
@@ -123,7 +123,7 @@ def build_shap_values():
     use_local_file = False
     if use_local_file:
         shap_values = pandas.read_csv(
-            "./data/shap_values-2024.csv",
+            "./data/shap_values-2025.csv",
             sep=conf.data.shap_values.sep,
             encoding=conf.data.shap_values.encoding,
             compression=None,
@@ -133,7 +133,7 @@ def build_shap_values():
     else:
         download_shap_values()
         shap_values = pandas.read_csv(
-            "./data/shap_values-2024.csv.zip",
+            "./data/shap_values-2025.csv.zip",
             sep=conf.data.shap_values.sep,
             encoding=conf.data.shap_values.encoding,
             compression="zip",
@@ -145,7 +145,7 @@ def build_shap_values():
 
 @st.cache_data(ttl=3600)  # 1h cache
 def download_history():
-    date, url = artifacts.get_last_artifact("history-2024.csv")
+    date, url = artifacts.get_last_artifact("history-2025.csv")
     logger.debug(f"Downloading history from {url}")
     download.download_data_from_url_to_file(
         url, "./data/history-artifact.csv.zip", auth=artifacts.get_github_auth()
