7f5de6616085542268245b23112380f041546486
pauldes <pauldesigaud@outlook.fr>
2026-04-07 16:05:40 +0200

    Linting

--- nba_mvp_predictor/web.py
+++ nba_mvp_predictor/web.py
@@ -470,12 +470,10 @@ def run():
             datetime.now().year + 1 if datetime.now().month > 9 else datetime.now().year
         )
 
-        st.markdown(
-            f"""
+        st.markdown(f"""
         *Predicting the NBA Most Valuable Player for the {current_season-1}-{str(current_season)[-2:]} season using machine learning.*
         *Last update : {last_update}.*
-        """
-        )
+        """)
 
         navigation_page = st.sidebar.radio(
             "Navigate to", [PAGE_PREDICTIONS, PAGE_EXPLICABILITY, PAGE_PERFORMANCE]
@@ -544,12 +542,10 @@ def run():
 
                 for n, player_name in enumerate(top_3):
                     title_level = "###" + n * "#"
-                    col1.markdown(
-                        f"""
+                    col1.markdown(f"""
                     ##### {emojis[n]} **{player_name}**
                     *{top_3[player_name]} probability to win MVP*
-                    """
-                    )
+                    """)
 
                 show_top_n = compute_probs_based_on_top_n
                 # show_top_n = min([compute_probs_based_on_top_n, 10])
@@ -700,13 +696,11 @@ def run():
                 col1.markdown(
                     f"##### All {num_test_seasons} seasons ({performances.index.min()}-{performances.index.max()})"
                 )
-                col1.markdown(
-                    f"""
+                col1.markdown(f"""
                 - **{percentage}** of MVPs correctly found
                 - The true MVP is ranked **{avg_real_rank}** by the model in average
                 - The true rank of the predicted MVP is **{avg_pred_rank}** in average
-                """
-                )
+                """)
                 # col2
                 performances_last10 = performances.head(10)
                 percentage = mvp_found_pct(performances_last10)
@@ -716,21 +710,17 @@ def run():
                 col2.markdown(
                     f"##### Last {num_test_seasons} seasons ({performances_last10.index.min()}-{performances_last10.index.max()})"
                 )
-                col2.markdown(
-                    f"""
+                col2.markdown(f"""
                 - **{percentage}** of MVPs correctly found
                 - The true MVP is ranked **{avg_real_rank}** by the model in average
                 - The true rank of the predicted MVP is **{avg_pred_rank}** in average
-                """
-                )
+                """)
 
                 st.dataframe(data=performances, width=None, height=None)
-                st.markdown(
-                    """
+                st.markdown("""
                 Predictions of the model are made on the unseen season using holdout method.
                 Players with no MVP vote are considered as ranked 10th for simplification.
-                """
-                )
+                """)
 
             else:
                 st.warning(
@@ -786,22 +776,18 @@ def run():
                     "👍 Stats with the strongest **positive impact** on the model prediction for this player:"
                 )
                 for i, col in enumerate(st.columns(num_stats)):
-                    col.success(
-                        f"""
+                    col.success(f"""
                         **{(top_features_positive_impact[i])}**   
                         *+{round(top_features_positive_impact_values[i], 2)} MVP share*
-                        """
-                    )
+                        """)
                 st.markdown(
                     "👎 Stats with the strongest **negative impact** on the model prediction for this player:"
                 )
                 for i, col in enumerate(st.columns(num_stats)):
-                    col.error(
-                        f"""
+                    col.error(f"""
                         **{top_features_negative_impact[i]}**    
                         *{round(top_features_negative_impact_values[i], 2)} MVP share*
-                        """
-                    )
+                        """)
 
                 st.subheader("Global explanation")
                 st.markdown(
